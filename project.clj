c3c82a35a1f0660afff1a754dda897446a0b4335
Patrick Carlisle <patrick@puppetlabs.com>
2014-08-05 12:44:01 -0700

    Fix deploy-repositories

--- project.clj
+++ project.clj
@@ -1,3 +1,10 @@
+(defn deploy-info
+  [url]
+  {:url url
+   :username :env/nexus_jenkins_username
+   :password :env/nexus_jenkins_password
+   :sign-releases false})
+
 (defproject puppetlabs/schema-tools "0.1.0-SNAPSHOT"
   :description "Tools for working with prismatic schema"
   :url "http://github.com/puppetlabs/clj-schema-tools"
@@ -6,10 +13,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [prismatic/schema "0.2.6"]]
 
-  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
-                                     :username :env/clojars_jenkins_username
-                                     :password :env/clojars_jenkins_password
-                                     :sign-releases false}]]
+  :deploy-repositories [["releases" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/releases/")]
+                        ["snapshots" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/")]]
 
   :lein-release {:scm :git
                  :deploy-via :lein-deploy}
